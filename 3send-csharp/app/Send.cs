02bd1d2b4564e54fc1858a73daf8ffe8c1638bd7
Jujhar Singh <jujhar@jujhar.com>
2016-10-13 13:41:28 +0100

    csharp example working

--- 3send-csharp/app/Send.cs
+++ 3send-csharp/app/Send.cs
@@ -4,22 +4,27 @@ using System.Text;
 
 class Send
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        var factory = new ConnectionFactory() { HostName = "localhost" };
+        var factory = new ConnectionFactory() { HostName = "rabbit" };
+
         using(var connection = factory.CreateConnection())
         using(var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
+            //build string, defailt to hello world if not passed in
             string message = "Hello World!";
+            if (args.Length != 0){
+                message = String.Join(" ",args);
+            }
+
             var body = Encoding.UTF8.GetBytes(message);
 
             channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
             Console.WriteLine(" [x] Sent {0}", message);
         }
 
-        Console.WriteLine(" Press [enter] to exit.");
-        Console.ReadLine();
+        Environment.Exit(1);
     }
 }
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ To get the demo working, run the containers in the following order
 
 ### 1. RabbitMQ
 ```bash
-    docker run --hostname rabbit --name myrabbit -p 9080:15672 rabbitmq:3-management
+    docker run --hostname rabbit --name rabbit -p 9080:15672 rabbitmq:3-management
 ```
 
 ### 2. MySQL
@@ -17,7 +17,7 @@ To get the demo working, run the containers in the following order
 
 ### 3. [Java[Send message to RabbitMQ `/3second-python`
 ```
-    docker build -t mono ./ && docker run --link myrabbit mono
+    docker build -t mono ./ && docker run --link rabbit mono
 ```
 
 ### 4. Receive message in Node from RabbitMQ and write to Mysql
